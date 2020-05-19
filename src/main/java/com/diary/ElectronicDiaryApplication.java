9ac941f2536515c6e44cbb64557ac4dd44a73d35
kaszakinga90@gmail.com <kaszakinga90@gmail.com>
2020-05-19 11:54:03 +0200

    First commit

---
+++ src/main/java/com/diary/ElectronicDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.diary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicDiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectronicDiaryApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/diary/Firstclass.java
@@ -0,0 +1,4 @@
+package com.diary;
+
+public class Firstclass {
+}
